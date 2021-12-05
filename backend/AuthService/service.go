219ace338b819b196b8d46b641db1c985c098cf1
jalexanderII <a.joeljr@hotmail.com>
2021-12-05 14:37:01 -0500

    proto for login

---
+++ backend/AuthService/service.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/jalexanderII/silver-palm-tree/proto"
+	"google.golang.org/grpc"
+)
+
+type authServer struct {}
+
+func(authServer) Login(_ context.Context, in *proto.LoginRequest) (*proto.AuthResponse, error){
+	return &proto.AuthResponse{}, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	proto.RegisterAuthServiceServer(server, authServer{})
+	listener, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal("Error creating listener: ", err.Error())
+	}
+	server.Serve(listener)
+}
